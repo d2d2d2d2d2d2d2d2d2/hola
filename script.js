c2bc7806b378aa05fc3ea1019ee477c5757a93fa
d2d2d2d2d2d2d2d2d2 <160093150+d2d2d2d2d2d2d2d2d2@users.noreply.github.com>
2024-02-14 23:24:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+        
+        setTimeout(function() {
+            window.location.href = 'Carta.html';
+          }, 2000);
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+   
+});
